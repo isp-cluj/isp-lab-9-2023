87039cc8797963eebdb1a04f1720ab4f5d1e739a
Radu Miron <radu.miron@aut.utcluj.ro>
2024-04-23 15:26:46 +0300

    fixed typo in class name
    Signed-off-by: Radu Miron <radu.miron@aut.utcluj.ro>

--- src/main/java/isp/lab9/exercise1/ui/PortfolioJPanel.java
+++ src/main/java/isp/lab9/exercise1/ui/PortfolioJPanel.java
@@ -6,6 +6,6 @@ import javax.swing.*;
  *  todo: implement portfolio panel; for each owned stock add symbol, quantity, price per unit, total price of the position
  *  it should look similar to the 'Market' panel
  */
-public class PortofolioJPanel extends JPanel {
+public class PortfolioJPanel extends JPanel {
 
 }
